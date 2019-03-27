1da093c84359b43cfde12b61cdd1121de49627a3
SimpleBento <rlalli@sfu.ca>
2019-03-26 22:46:01 -0700

    Updated Project Folder Structure and Locomotion

---
+++ Assets/_Scripts/ChangeColorOnHover.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorOnHover : MonoBehaviour {
+
+
+    Material m_Material;
+    Color32 s_Material;
+
+    // Use this for initialization
+    void Start () {
+        //Fetch the Material from the Renderer of the GameObject
+        m_Material = GetComponent<Renderer>().material;
+        s_Material = GetComponent<Renderer>().material.color;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        m_Material.color = Color.black;
+        Debug.Log("OnObject");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_Material.color = s_Material;
+        Debug.Log("Exit");
+    }
+
+
+
+}
---
+++ Assets/_Scripts/WeaponScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour {
+
+    // Use this for initialization
+
+
+
+    float maxDistance = 1000;
+
+    void Start () {
+
+
+	}
+
+
+
+
+    //check for objects with tag Destroyable, if raycast hits said object with collider, it will be destoryed. 
+    void FixedUpdate()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position, fwd * 1000, Color.green);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, fwd, out hit, maxDistance) && hit.transform.tag == "Destroyable")
+        {
+           // hit.transform.gameObject.GetComponent<Renderer>().material.color = Color.black;
+        }
+
+
+        if (OVRInput.Get(OVRInput.RawAxis1D.RIndexTrigger) > 0.0f)
+        {
+
+            if (Physics.Raycast(transform.position, fwd, out hit, maxDistance) && hit.transform.tag == "Destroyable")
+            {
+                print("This object can be destroyed");
+                Destroy(hit.transform.gameObject);
+
+            }
+        }
+    }
+
+ 
+
+
+
+
+}
